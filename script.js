e2ef9e10c5abfaf27601a83c88407d3a64a51a48
Joel D'Autilio <joeldautilio@gmail.com>
2023-01-06 19:32:57 -0300

    Implement add book functionality

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
-let myLibrary = [];
-
-function Book(title, author, read) {
-  this.title = title;
-  this.author = author;
-  this.read = read ? read : false;
+class Book {
+  constructor(title, author, read) {
+    this.title = title;
+    this.author = author;
+    this.read = read ? read : false;
+  }
 }
+
+let myLibrary = [];
 
 function addBookToLibrary(title, author, read) {
   let newBook = new Book(title, author, read);
   myLibrary.push(newBook);
+  addBookCard(newBook);
 }
 
-addBookToLibrary("A Passage to India", "E. M. Foster");
-addBookToLibrary("Daisy Jones and the Six", "Taylor Jenkins Reid");
-addBookToLibrary("Carmilla", "Sherdain Le Fanu");
-addBookToLibrary("Sapiens: A Brief History of Humankind", "Yuval Noah Harari");
-addBookToLibrary("At the Precipice", "R. R. Quan");
-addBookToLibrary("Sanctuary", "William Faulkner");
-
 const cardContainer = document.querySelector(".card-container");
+const showFormButton = document.querySelector("#show-form-button");
+const newBookForm = document.querySelector("#new-book-form");
 
 myLibrary.forEach(addBookCard);
+
+showFormButton.addEventListener("click", showForm);
+
+window.addEventListener("click", hideForm);
+
+newBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const read = document.querySelector("#read").checked;
+
+  addBookToLibrary(title, author, read);
+  hideForm();
+});
+
+function showForm() {
+  newBookForm.classList.remove("hidden");
+}
+
+function hideForm(e) {
+  if (e != undefined) {
+    if (newBookForm.classList.contains("hidden")) return;
+    if (e.target == showFormButton) return;
+    if (newBookForm.contains(e.target)) return;
+  }
+
+  newBookForm.classList.add("hidden");
+}
 
 function addBookCard(book) {
   let card = document.createElement("div");
   cardContainer.appendChild(card);
-  card.classList.add("card")
+  card.classList.add("card");
 
   let title = document.createElement("p");
   card.appendChild(title);
   title.classList.add("book-title");
   title.textContent = book.title;
-  
+
   let author = document.createElement("p");
   card.appendChild(author);
   author.classList.add("author");
